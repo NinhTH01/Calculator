4cbd3fb54e5d599bf74a8f812e3e8aaf84466f8e
Trần Hải Ninh <hninh1710@gmail.com>
2022-03-15 16:07:54 +0700

    'Added'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Calculator from './component/calculator/calculator';
+
+function App() {
+    return (
+      <div>
+           <Calculator/>
+      </div>
+    );
+}
+
+export default App;
